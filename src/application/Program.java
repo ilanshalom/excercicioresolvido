e3bb69a1ca77f97bfc928c7c5e55f4b1315d8db6
Ilan Garcia <ilanelvaron24@gmail.com>
2024-04-10 21:10:24 -0300

    atual

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import model.entities.Reservation;
+
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
@@ -19,8 +21,33 @@ public class Program {
 		
 		System.out.print("check-out date (dd/MM/yyyy):");
 		Date checkOut = sdf.parse(sc.next());
+		
 		if (!checkOut.after(checkIn)) {
 			System.out.println(" Errorin reservation: Check-out date must beaftercheck-in date");
+		}
+		else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation"  + reservation);
+			System.out.println("");
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Room number: ");
+			System.out.print("check-in date (dd/MM/yyyy):");
+			 checkIn = sdf.parse(sc.next());
+			
+			System.out.print("check-out date (dd/MM/yyyy):");
+			 checkOut = sdf.parse(sc.next());
+			 
+			 Date now = new Date();
+			 if(checkIn.before(now) || checkOut.before(now)) {
+				 System.out.println(" Errorin reservation: Reservation dates for updatemust be future dates");
+			 }
+			 else if (!checkOut.after(checkIn)) {
+			System.out.println(" Errorin reservation: Check-out date must beaftercheck-in date");	 
+			 }
+			 else {
+			 reservation.updateDates(checkIn, checkOut);
+			 System.out.println("Reservation"  + reservation);
+		 }
 		}
     sc.close();
 	}
